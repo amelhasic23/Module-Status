4aebcfa76007f2d53c7b6c6806d7f8a612074987
amelhasic23 <117470943+amelhasic23@users.noreply.github.com>
2024-11-19 10:37:19 +0100

    Commit

---
+++ Module Status/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercises
+{
+    class ModuleStatus
+    {
+        static void Main(string[] args)
+        {
+            bool status = true;
+
+            Console.WriteLine($"Module status: {status}");
+        }
+    }
+}
